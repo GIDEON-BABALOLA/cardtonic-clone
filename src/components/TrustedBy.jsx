5a0fa598069224d0f479c0de33c1a38e03374e87
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-05-08 13:17:47 +0100

    you

--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const TrustedBy = () => {
   return (
-   <div className='w-full lg:gap-0 gap-9 px-[30px] lg:px-[35px] py-[80px] lg:flex-wrap flex flex-row items-center sm:justify-between justify-center '>
+   <div className='w-full lg:gap-0 gap-9 px-[30px] lg:px-[35px] py-[80px] md:flex-nowrap flex-wrap flex flex-row items-center sm:justify-between justify-center '>
 <img className='w-1xl' src='https://ik.imagekit.io/rwgk2b4rf/svgexport-17.svg?updatedAt=1746608985825'/>
 <img className='max-w-[150px]' src='https://ik.imagekit.io/rwgk2b4rf/naija.webp?updatedAt=1746704378784' />
 <img className=' w-[80px] lg:w-auto' src='https://ik.imagekit.io/rwgk2b4rf/svgexport-18.svg?updatedAt=1746608985851' />
