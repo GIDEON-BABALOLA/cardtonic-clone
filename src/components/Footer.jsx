1af735de62d076ff56e0f009bce273301a8f0ac0
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-05-08 17:08:36 +0100

    lets keep going

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Footer = () => {
+  const [accordionOpen, setAccordionOpen] = useState(false)
+  const footerData = [
+    {
+      title : "Features",
+      content : [
+        "Buy Gift Cards",
+        "Sell Gift Cards",
+        "Dollar Card",
+        "Just Gadgets",
+        "Bill Payments"
+      ]
+    },
+    {
+      title : "Products",
+      content : [
+        "Airtime",
+        "Data",
+        "Electricity",
+        "TV Subscription",
+        "Apple"
+      ]
+    },
+    {
+      title : "Available Gift Cards",
+      content :  [
+        "Travel Gift Cards",
+        "Lifestyle Gift Cards",
+        "Music Gift Cards",
+        "Supermarket Gift Cards",
+        "Gaming Gift Cards"
+      ]
+    },
+    {
+      title : "Resources",
+      content : [
+        "Ghanaians",
+        "Blog",
+        "Rewards",
+        "Gift Card Rates",
+        "Developer API"
+      ]
+    },
+    {
+      title : "Company",
+      content : [
+        "Partners",
+        "#CTGivesBack",
+        "Upskill",
+        "Privacy Policy",
+        "Terms of Use"
+      ]
+    },
+    {
+      title : "Connect",
+      content :  [
+        "Get In Touch",
+        "support@cardtonic.com",
+        "+234 (0) 1 343 0626",
+        "+234 (0) 706 050 2770"
+      ]
+    }
+  ]
+  const toggleAccordion = (index) => {
+    setAccordionOpen((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
   return (
     <>
-    <section className='flex flex-row  w-full px-30 items-start gap-[25px] py-20'>
+    <section className=' hidden lg:flex flex-row  w-full px-30 items-start gap-[25px] py-20'>
 <div className='flex flex-col justify-between gap-7 w-full'>
 <span className='text-[#1B507E] font-semibold'>Features</span>
 <span className="text-[16px] text-[#1B507E]">Buy Gift Cards</span>
@@ -58,6 +126,34 @@ const Footer = () => {
 <span className="text-[16px] text-[#1B507E]">+234 (0) 1 343 0626</span>
 <span className="text-[16px] text-[#1B507E]">+234 (0) 706 050 2770</span>
 </div>
+    </section>
+
+    <section className='lg:hidden flex flex-col w-full
+ justify-between items-center bg-white'>
+      {
+        footerData.map((content, index) => (
+          <div key={index} className='justify-between items-center  min-w-[300px] py-[50px]'>
+          <button
+         onClick={() => toggleAccordion(index)}
+          className='flex justify-between w-full'>
+            <span className='text-[#1B507E] font-semibold'>{content.title}</span>
+            { accordionOpen[index] ? <span className='text-3xl text-[#1B507E]'>-</span>
+            : <span className='text-3xl text-[#1B507E]'>+</span> }
+          </button>
+          <div className={`grid overflow-hidden transition-all duration-200
+            ease-in-out text-slate-600 text-sm
+            ${accordionOpen[index] ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"}`}>
+            <div className='overflow-hidden flex flex-col justify-between gap-[30px] mt-10'>
+              {content.content.map((item, index) => (
+                <span className="text-[#1B507E]" key={index}>{item}</span>
+              ))}
+            </div>
+          </div>
+            
+     </div>
+        ))
+      }
+
     </section>
     <section className='flex  flex-col lg:flex-row w-full px-30 text-[#1B507E]  items-center bg-[#F8F8F9] py-[50px]  gap-[10px]'>
       <div className='w-full text-center'>
